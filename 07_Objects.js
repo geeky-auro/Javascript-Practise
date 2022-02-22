c7d8294048218b73c59fefdb0ddedb3534f187b2
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-02-22 12:59:43 +0530

    Objects.js

--- 07_Objects.js
+++ 07_Objects.js
@@ -12,3 +12,36 @@ document.write("<h1>Document Object</h1>");
 document.writeln("To be written in a new line! with the help of writeln <br>");
 document.write("To be written in a new line! not with the help of writeln");
 // Window Object
+function openwindow(){
+open("/home.html","Home")
+
+}
+
+function closewindow(){
+  close();
+}
+
+// Window object supports 3 types of message boxes
+
+function Alertonly(){
+  window.alert("Hit Ok to be safe..!");
+}
+
+function Confirmonly(){
+  window.confirm("Select your preferences to proceed..!")
+}
+
+function Promptonly(){
+  var getpassword=window.prompt("You are requested to Enter the Pwd","Password");
+  var getpassword1=window.prompt("You are requested to Enter the Pwd","Password");
+  
+  if (getpassword != null) {
+    document.getElementById("demo").innerHTML =
+    "Hello " + getpassword + "! How are you today?";
+  }
+  
+  document.write("<br>");
+  document.write("Entered Password is "+getpassword);
+  document.write("<br>");
+  document.write("Entered Password is "+getpassword1);
+}
