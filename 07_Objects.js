660f14a46ea111ea0ac262e4b0aa1ad46e722460
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-02-22 11:43:24 +0530

    Objects.js

---
+++ 07_Objects.js
@@ -0,0 +1,12 @@
+// The predefined objects that are commonly used are the window and document objects.
+/*SOme objects are
+Document
+Window
+Browser
+Form
+Math
+Date
+*/
+// Document Object
+document.write("<body><h1>Document Object</h1></body>");
+document.writeln("To be written in a new line!");
