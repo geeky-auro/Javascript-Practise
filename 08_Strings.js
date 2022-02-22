e58ab1934fbc30f437430a3306766638db271685
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-02-22 20:40:21 +0530

    strings.js

--- 08_Strings.js
+++ 08_Strings.js
@@ -1,3 +1,6 @@
 var namei="Auro Saswat raj";
 document.write(namei);
 document.write("<br> Add some more lines too with a break <br>");
+// Creating String by using new keyword.>!
+var string1=new String("Sugar is hard");
+document.writeln(string1);
