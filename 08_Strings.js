7f38f94e58e5d6975e88aeb7d44857c4e9aea20b
Auro Saswat Raj <aurosaswat.apk@gmail.com>
2022-02-22 21:05:49 +0530

    strings.js

--- 08_Strings.js
+++ 08_Strings.js
@@ -4,3 +4,7 @@ document.write("<br> Add some more lines too with a break <br>");
 // Creating String by using new keyword.>!
 var string1=new String("Sugar is hard");
 document.writeln(string1);
+document.write("<br>");
+document.write("Use of CharAt function: "+string1.charAt(2));
+document.write("<br>");
+document.write("Make use of concatenation "+string1.concat(" Tissue"));
